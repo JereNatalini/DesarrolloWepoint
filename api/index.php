f2e03eeaeccd8ca69759570d5cc0e3d7208c044b
juansantacroce <juansantacroce@gmail.com>
2024-02-09 11:44:29 -0300

    Intentando agregar metodo SaleOrder en index

--- api/index.php
+++ api/index.php
@@ -88,6 +88,15 @@
 
         insertOrdenDeCompra($purchase_order['vendor_id'], $JsonPurchaseorder);
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+        
+
+        //Faltaria crear un boton que llame a la funcion de Post zoho y creee una orden de compra 
+
+
+        //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
         Flight::json(['status' => 'success']);
     });
 
@@ -110,16 +119,59 @@
         $statement->execute();
     }
 
+    Flight::route('POST /so', function(){
+        $body_sale_order = Flight::request(); //validar atributos del body
+        $sale_order_data = json_decode($$body_sale_order->getBody(), true);
 
-    //Orden para insertar orden de compra
-    function insertOrdenDeCompra ($id_Cliente, $purchase_order){
-        $statement = Flight::db()->prepare('INSERT INTO Orden_de_compra (Id_cliente, orden_de_compra) VALUES (?, ?)');
-        $statement->bindParam(1, $id_Cliente, PDO::PARAM_STR);
-        $statement->bindParam(2, $purchase_order, PDO::PARAM_STR);
+        if (!isset($sale_order_data['line_items'])) {
+            echo json_encode(['error' => 'No se encontraron items en la Factura']);
+            return;
+        }
+
+        $sale_order = new SoBuilder();
+        $sale_order->set('customer_id', $sale_order_data['customer_id']);
+        $sale_order->set('date', $sale_order_data['date']);
+        $sale_order->set('line_items', $sale_order_data['line_items']);
+
+        foreach ($sale_order_data['line_items'] as $item_data) {
+            
+            //Verificar si el item existe en la base de datos
+            $existing_item = getItem($item_data['sku']);
+
+            if(!$existing_item) {
+                //primero deberia evaluar la posibilidad de que este cargado en el zoho y no en la db
+                error_log('El producto ' . $item_data['name'] . 'nunca se recibio en una Orden de Compra');
+            }
+            else{
+
+                //BUILDEAR ITEM
+                $item_builder = new ItemBuilder();
+                $item_builder->set('name', $existing_item['name']);
+                $item_builder->set('sku', $existing_item['sku']);
+                $item_builder->set('description', $existing_item['description']);
+                $item_builder->set('unit', $existing_item['unit']);
+                $item_builder->set('quantity', $sale_order_data['quantity']);
+                $item_builder->set('item_id_zoho', $existing_item['item_id_zoho']);
+
+
+
+                $sale_order->addItem($item_builder->buildItem());
+            }
+            
+        }
+        Flight::start();
+    });
+
+    //ADAPTAR METODO A DB
+    function insertSaleOrder($saleOrder){
+        $statement = Flight::db()->prepare('INSERT INTO Items (sku, item_name, item_desc, unit) VALUES (?, ?, ?, ?)');
+        $statement->bindParam(1, $item_data['sku'], PDO::PARAM_STR);
+        $statement->bindParam(2, $item_data['name'], PDO::PARAM_STR);
+        $statement->bindParam(3, $item_data['description'], PDO::PARAM_STR);
+        $statement->bindParam(4, $item_data['unit'], PDO::PARAM_STR);
         $statement->execute();
     }
-
-
+    
     Flight::start();
 
 ?>
