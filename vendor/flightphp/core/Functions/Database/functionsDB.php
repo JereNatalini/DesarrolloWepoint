f0c56f6164d8e460635f5260feb64045a284fc45
Agustin-Salinas <salinas.agus.2000@gmail.com>
2024-04-15 20:32:03 -0300

    Comentario

--- vendor/flightphp/core/Functions/Database/functionsDB.php
+++ vendor/flightphp/core/Functions/Database/functionsDB.php
@@ -1,1 +1,6 @@
 <!-- Funciones propias de la base de datos -->
+
+
+
+
+<!-- Aca no se si poner la cadena de coneccion a la base de datos y todos los script de SQL para hacerlo mas seguro y comodo pa nosotros  --> 
